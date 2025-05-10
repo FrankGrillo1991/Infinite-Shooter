1ebeafe848216ee741b861ad6b7c9f2a3e907377
FrankGrillo1991 <fsg12491@aol.com>
2025-05-10 18:25:12 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,3 +4,12 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let player = {
+    x: canvas.width / 2,
+    y: canvas.height - 80,
+    width: 40,
+    height: 40,
+    color: "lime",
+    speed: 5
+};
+
